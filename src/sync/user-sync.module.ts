7cfe93f070bb374c0ffd3513806d21f19336df44
Strange Signal <signal@strangesignal.studio>
2026-02-27 22:42:03 -0500

    fix: UserSyncModule must import RelayDiscoveryModule

--- src/sync/user-sync.module.ts
+++ src/sync/user-sync.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserSyncService } from './user-sync.service';
+import { RelayDiscoveryModule } from '../relay-discovery/relay-discovery.module';
 
 @Module({
+  imports: [RelayDiscoveryModule],
   providers: [UserSyncService],
   exports: [UserSyncService],
 })
